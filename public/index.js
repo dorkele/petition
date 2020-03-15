ba9535b46a69222acdebe458bda074884810a2dc
dorkele <dora_tominic@hotmail.com>
2020-03-15 21:04:14 +0100

    fixed canv

--- public/index.js
+++ public/index.js
@@ -5,21 +5,19 @@
     var canvas = document.getElementById("canv"); /////////////zasto ovo ne funkcionira s jquery?? ako sve bude radilo u vanilla js probati prebaciti u jquery
     var ctx = canvas.getContext("2d");
 
-    var startPos = {
+    var start = {
         x: 0,
         y: 0
     };
-    /////////////ove funkcije ispod mozda promijeniti u promises umjesto callbacka - ako je moguce????
-    canvas.addEventListener("mousemove", draw);
-    canvas.addEventListener("mousedown", startPosition);
-    canvas.addEventListener("mouseenter", startPosition);
 
     function startPosition(e) {
-        startPos.x = e.clientX - canvas.offsetLeft;
-        startPos.y = e.clientY - canvas.offsetTop;
+        start.x = e.clientX - canvas.offsetLeft;
+        start.y = e.clientY - canvas.offsetTop;
     }
 
-    function draw(e) {
+    canvas.addEventListener("mousedown", startPosition);
+
+    canvas.addEventListener("mousemove", function draw(e) {
         if (e.buttons !== 1) {
             return;
         } else {
@@ -27,20 +25,16 @@
             ctx.strokeStyle = "violet";
             ctx.lineWidth = 2;
 
-            ctx.moveTo(startPos.x, startPos.y);
+            ctx.moveTo(start.x, start.y);
             startPosition(e);
-            ctx.lineTo(startPos.x, startPos.y);
+            ctx.lineTo(start.x, start.y);
             ctx.stroke();
         }
-    }
+    });
 
     var canvInput = document.getElementById("canv-input");
-    var canvasInput = "";
 
     canvas.addEventListener("mouseup", function() {
         canvInput.value = canvas.toDataURL();
-        canvasInput += canvInput.value;
-        return canvasInput;
     });
-    console.log(canvasInput);
 })();
